3655649d2e02a751d1d4fa590ea62e06617a8a24
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-04-23 10:47:58 -0500

    refactor: :recycle: Rename users controller file

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import * as UsersController from '../controllers/user.controller.js';
+import * as UsersController from '../controllers/users.controller.js';
 
 const router = Router();
 
